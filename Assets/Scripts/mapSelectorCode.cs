f53fc9112d589617a090eb71a2b94b3f1afe231a
Skiller <111735329+ArcaneP@users.noreply.github.com>
2023-04-04 14:30:28 +0200

    level selector

--- Assets/Scripts/mapSelectorCode.cs
+++ Assets/Scripts/mapSelectorCode.cs
@@ -9,28 +9,64 @@ public class mapSelectorCode : MonoBehaviour
 
     public GameObject iconOBJ;
 
-    private void Awake()
+    public GameObject buttonLeft, buttonRight;
+
+    private void Start()
     {
         levelPointer = 0;
         iconOBJ.GetComponent<Image>().sprite = levelList.levels[levelPointer];
 
         Debug.Log("levellist is: " + levelList.levels.Length);
+
+        if(levelPointer == 0)
+        {
+            buttonLeft.gameObject.SetActive(false);
+        }
     }
 
     public void rightButton()
     {
+
         if (levelPointer < levelList.levels.Length - 1)
-        {         
+        {
             levelPointer++;
             Debug.Log(levelPointer);
             iconOBJ.GetComponent<Image>().sprite = levelList.levels[levelPointer];
         }
 
+        if (levelPointer > 0)
+        {
+            buttonLeft.gameObject.SetActive(true);
+        }
+
         if(levelPointer == levelList.levels.Length - 1)
         {
-            levelPointer = 0;
-            iconOBJ.GetComponent<Image>().sprite = levelList.levels[levelPointer];
+            buttonRight.gameObject.SetActive(false);
         }
+
+    }
+    public void leftButton()
+    {
+        if (levelPointer < levelList.levels.Length - 1 || levelPointer < levelList.levels.Length)
+        {
+            if(levelPointer >= 0)
+            {
+                levelPointer--;
+            }
+                Debug.Log(levelPointer);
+                iconOBJ.GetComponent<Image>().sprite = levelList.levels[levelPointer];
+        }
+
+        if (levelPointer == 0)
+        {
+            buttonLeft.gameObject.SetActive(false);
+        }
+
+        if (levelPointer < levelList.levels.Length - 1)
+        {
+            buttonRight.gameObject.SetActive(true);
+        }
+
     }
 
 }
