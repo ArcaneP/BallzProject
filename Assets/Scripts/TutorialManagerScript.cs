c08635db881c469eed72bfcced0e771f8600b82d
Arcane P <111735329+ArcaneP@users.noreply.github.com>
2023-08-16 13:38:55 +0200

    small fixes

--- Assets/Scripts/TutorialManagerScript.cs
+++ Assets/Scripts/TutorialManagerScript.cs
@@ -1,173 +1,105 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialManagerScript : MonoBehaviour
 {
-    [SerializeField] GameObject PlayButton,LevelButton, OptionsButton, DemoButton, tutButton;
-
+    [SerializeField] GameObject PlayButton, LevelButton, OptionsButton, DemoButton, tutButton;
     [SerializeField] bool hasCompletedTutorial;
-
-
     [SerializeField] GameObject hp, video, howto;
-
     [SerializeField] int currentStep = 0;
-    bool step1Completed = false;
-    bool step2Completed = false;
-    bool step3Completed = false;
+    bool[] stepCompleted = new bool[4];
 
     private void Start()
-    {      
-        if(PlayerPrefs.GetFloat("hasCompletedTutorial") == 0)
-        {
-            hasCompletedTutorial = false;
-        }
-        else
-        {
-            hasCompletedTutorial = true;
-        }
+    {
+        hasCompletedTutorial = PlayerPrefs.GetFloat("hasCompletedTutorial") != 0;
 
         if (!hasCompletedTutorial)
         {
-
             StartTutorial();
             NextStep();
         }
         else
         {
-
             DemoButton.SetActive(false);
-
-            this.GetComponentInChildren<Canvas>().gameObject.SetActive(false);
-            Debug.Log("Tutorial has already been complete");
-
-            
+            GetComponentInChildren<Canvas>().gameObject.SetActive(false);
+            Debug.Log("Tutorial has already been completed");
         }
     }
 
     public void StartTutorial()
     {
-        PlayerPrefs.SetInt("lastSceneName",1);
+        PlayerPrefs.SetInt("lastSceneName", 1);
         PlayerPrefs.SetInt("hp", 1);
-        GameManager.Instance.curHealth = 1; 
+        GameManager.Instance.curHealth = 1;
         GameManager.Instance.HealPlayer(3);
         TimerScript.Instance.enabledTimer = false;
 
-        // Show initial instructions/UI elements
-        // Activate necessary game objects
-        // Set up any initial conditions
-
-        if (PlayButton != null)
-        {
-            PlayButton.SetActive(false);
-        }
-
-        if(LevelButton != null)
-        {
-            LevelButton.SetActive(false);
-        }
-
-        if (OptionsButton != null)
-        {
-            OptionsButton.SetActive(false);
-        }
-
-        if(DemoButton != null)
-        {
-            DemoButton.SetActive(false);
-        }
-
-
+        PlayButton?.SetActive(false);
+        LevelButton?.SetActive(false);
+        OptionsButton?.SetActive(false);
+        DemoButton?.SetActive(false);
     }
 
-    // Call this function to proceed to the next step
     public void NextStep()
     {
-        currentStep++;
-
-        // Check the current step and perform actions accordingly
-        switch (currentStep)
+        if (currentStep < stepCompleted.Length)
         {
-            case 1:
-                if (!step1Completed)
-                {
-                    // Execute Step 1 logic
-                    // Show instructions for Step 1
-                    hp.SetActive(true);
-                    Debug.Log("step1 Completed");
-                }
-                else
-                {
-                    // Step 1 already completed, move to the next step
-                    NextStep();
-                }
-                break;
-            case 2:
-                if (!step2Completed)
-                {
-                    // Execute Step 2 logic
-                    // Show instructions for Step 2
-                    hp.SetActive(false);
-                    video.SetActive(true);
-                    Debug.Log("step2 Completed");
-                }
-                else
-                {
-                    // Step 2 already completed, move to the next step
-                    NextStep();
-                }
-                break;
-            case 3:
-                if (!step3Completed)
-                {
-                    // Execute Step 3 logic
-                    // Show instructions for Step 3
-                    video.SetActive(false);
-                    tutButton.SetActive(false);
+            currentStep++;
 
-                    howto.SetActive(true);
-                    if(DemoButton != null)
-                    {
+            while (currentStep <= stepCompleted.Length && stepCompleted[currentStep - 1])
+            {
+                currentStep++;
+            }
+
+            if (currentStep <= stepCompleted.Length)
+            {
+                switch (currentStep)
+                {
+                    case 1:
+                        hp.SetActive(true);
+                        Debug.Log("step1 Completed");
+                        break;
+                    case 2:
+                        hp.SetActive(false);
+                        video.SetActive(true);
+                        Debug.Log("step2 Completed");
+                        break;
+                    case 3:
+                        video.SetActive(false);
+                        tutButton.SetActive(false);
+                        howto.SetActive(true);
+                        Invoke("DisableHowTo", 1.5f);
+                        Debug.Log("step3 Completed");
                         DemoButton.SetActive(true);
-                    }
-
-                    Debug.Log("step3 Completed");
+                        break;
+                    //case 4:
+                    //    howto.SetActive(false);
+                    //    video.SetActive(false);
+                    //    tutButton.SetActive(false);
+                    //    Debug.Log("step4 Completed");
+                    //    break;
+                    default:
+                        break;
                 }
-                else
-                {
-
-                }
-                break;
-
-            default:
-
-                break;
+            }
         }
     }
 
-    // Call this function when a step is completed
+    private void DisableHowTo()
+    {
+        howto.GetComponent<Image>().enabled = false;
+    }
+
     public void CompleteStep(int step)
     {
-        switch (step)
+        if (step >= 1 && step <= stepCompleted.Length)
         {
-            case 1:
-                step1Completed = true;
-                Debug.Log("step1 Completed");
-                break;
-            case 2:
-                step2Completed = true;
-                Debug.Log("step2 Completed");
-                break;
-            case 3:
-                step3Completed = true;
-                Debug.Log("step3 Completed");
-                break;
-            // Add cases for additional steps
-            default:
-                break;
+            stepCompleted[step - 1] = true;
+            Debug.Log($"step{step} Completed");
         }
 
-        // Move to the next step
         NextStep();
     }
 
@@ -178,13 +110,10 @@ public class TutorialManagerScript : MonoBehaviour
         TimerScript.Instance.enabledTimer = false;
     }
 
-    public void ReDoTutorial() 
+    public void ReDoTutorial()
     {
         hasCompletedTutorial = false;
         PlayerPrefs.SetFloat("hasCompletedTutorial", 0);
-
         Debug.Log("can redo tutorial now");
     }
-
-
 }
